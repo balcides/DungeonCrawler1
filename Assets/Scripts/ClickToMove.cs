29ea20bf51834a5d93fe2ba37ef6a97017c64d8e
gabe <balcides@hotmail.com>
2019-03-20 23:33:31 -0700

    Lesson 1 Click to follow complete

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToMove : MonoBehaviour {
+
+	public float speed;
+	public CharacterController controller;
+
+	private Vector3 position; 
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//click to move
+		if(Input.GetMouseButton(0)){
+			
+			//locate where player clicked on the terrain
+			locatePosition();
+		}
+
+		//move the player to the position
+		moveToPosition();
+	}
+
+	void locatePosition(){
+		
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		if (Physics.Raycast (ray, out hit, 1000)) {
+			position = new Vector3 (hit.point.x, hit.point.y, hit.point.z);
+			Debug.Log (position);
+		}
+	}
+
+	void moveToPosition(){
+
+		//stop running if distance less than on in transform
+		if(Vector3.Distance(transform.position, position) > 1){
+			
+			Quaternion newRotation = Quaternion.LookRotation (position - transform.position);
+
+			newRotation.x = 0f;
+			newRotation.z = 0f;
+
+			transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, Time.deltaTime * 10);
+			controller.SimpleMove (transform.forward * speed);
+		}
+	}
+}

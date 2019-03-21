8b311b663d56c72b8841ab43a70eabfafd5c97ad
gabe <balcides@hotmail.com>
2019-03-21 02:52:08 -0700

    Chapter 3 - combat enemy follow
    click identify enemy on point.

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob : MonoBehaviour {
+
+	public float speed;
+	public float range;
+	public Transform player;
+	public CharacterController controller;
+
+	public AnimationClip run;
+	public AnimationClip idle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!inRange ()) {
+			chase ();
+		} else {
+			GetComponent<Animation> ().CrossFade (idle.name);
+		}
+
+	}
+
+	bool inRange(){
+
+		if (Vector3.Distance (transform.position, player.position) < range) { return true;
+		} else { return false; }
+	}
+
+	void chase(){
+		
+		transform.LookAt (player.position);
+		controller.SimpleMove (transform.forward * speed);
+		GetComponent<Animation> ().CrossFade(run.name);
+	}
+
+	void OnMouseOver(){
+
+		player.GetComponent<Fighter> ().opponent = gameObject;
+	}
+}
